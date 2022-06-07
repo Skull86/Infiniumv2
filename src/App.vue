4ef76757f6028e7ce81475091384754639ceaef4
Psycarlo <karkosyk@gmail.com>
2022-06-07 15:01:10 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="bg-brand-dark min-h-screen">
-    <h1 class="text-3xl font-bold underline text-brand-light">Hello world!</h1>
+  <div
+    class="bg-brand-dark min-h-screen flex flex-col items-center justify-center gap-6"
+  >
+    <h2 class="text-3xl font-bold text-brand-light">welcome to</h2>
+    <h1 class="text-8xl font-bold uppercase text-brand-light mb-8">infinium</h1>
+    <button class="bg-brand-light rounded-lg px-12 py-2 font-bold text-xl">
+      shop now
+    </button>
   </div>
 </template>
 
