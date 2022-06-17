e2fea9be0d818325d26e3c0a4aaf0d5910190230
Psycarlo <karkosyk@gmail.com>
2022-06-17 15:54:28 +0100

    Add showingQuestions logic

--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="flex flex-col justify-center items-center min-h-screen">
-    <h1
-      class="text-brand-dark font-bold text-5xl mb-10 cursor-pointer"
-      @click="showEasterEgg = !showEasterEgg"
+    <div
+      v-if="!showingQuestions"
+      class="w-full flex flex-col justify-center items-center"
     >
-      Infinium Survey
-    </h1>
-    <div class="flex flex-col w-1/5">
-      <p class="text-brand-dark font-bold">Username</p>
-      <input
-        class="focus:outline-none bg-brand-grey rounded-md px-2 py-1 mb-4"
-        placeholder="Optional"
-      />
-      <button
-        class="font-bold bg-brand-dark text-brand-light rounded-md py-1 hover:bg-brand-darkLight"
+      <h1
+        class="text-brand-dark font-bold text-5xl mb-10 cursor-pointer"
+        @click="showEasterEgg = !showEasterEgg"
       >
-        Start
-      </button>
+        Infinium Survey
+      </h1>
+      <div class="flex flex-col w-1/5">
+        <p class="text-brand-dark font-bold">Username</p>
+        <input
+          class="focus:outline-none bg-brand-grey rounded-md px-2 py-1 mb-4"
+          placeholder="Optional"
+        />
+        <button
+          class="font-bold bg-brand-dark text-brand-light rounded-md py-1 hover:bg-brand-darkLight"
+          @click="showingQuestions = true"
+        >
+          Start
+        </button>
+      </div>
+      <p v-if="showEasterEgg">Luís é o melhor programador do mundo!</p>
     </div>
-    <p v-if="showEasterEgg">Luís é o melhor programador do mundo!</p>
+    <div v-if="showingQuestions"></div>
   </div>
 </template>
 
@@ -26,4 +33,6 @@
   import { ref } from 'vue'
 
   const showEasterEgg = ref(false)
+
+  const showingQuestions = ref(false)
 </script>
